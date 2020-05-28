4d6e1f075c22c53fe310fcec095b4e89703e5111
mingyang.tan <mingyang.tan@mobvista.com>
2020-05-28 13:58:35 +0800

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const postRouter = require("./routes/post.js");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // 链接本地数据库
 // 第二个参数是为了消除警告
