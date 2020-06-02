9f9f7a17f8eb2fdb9b75e522d4afc9641ee83a47
mingyang.tan <mingyang.tan@mobvista.com>
2020-06-02 19:30:50 +0800

    add operate file

--- routes/file.js
+++ routes/file.js
@@ -8,11 +8,53 @@ const router = express.Router();
 router.get("/readFile", (req, res) => {
   fs.readFile(path.resolve(__dirname, "../public/fs-file.txt"), (err, data) => {
     if (err) {
-      console.error(err);
-      return;
+      res.send(err);
     }
     res.send(data.toString());
   });
+});
+
+// 打开文件
+router.get("/openFile", (req, res) => {
+  fs.open(path.resolve(__dirname, "../public/fs-file.txt"), "r+", (err, fd) => {
+    console.log(fd);
+    res.send("成功打开文件");
+  });
+});
+
+// 分字节读取文件
+router.get("/readBufFile", (req, res) => {
+  fs.open(path.resolve(__dirname, "../public/fs-file.txt"), "r+", (err, fd) => {
+    const buf = new Buffer.alloc(1024);
+    fs.read(fd, buf, 0, buf.length, 0, (err, bytes) => {
+      if (err) {
+        res.send(err);
+      }
+      res.send(buf.slice(0, bytes).toString());
+    });
+  });
+});
+
+// 获取文件信息
+router.get("/statFile", (req, res) => {
+  fs.stat(path.resolve(__dirname, "../public/fs-file.txt"), (err, stats) => {
+    res.send(stats.isFile());
+  });
+});
+
+// 写文件
+router.get("/writeFile", (req, res) => {
+  const data = JSON.stringify(req.query);
+  fs.writeFile(
+    path.resolve(__dirname, "../public/fs-file.txt"),
+    data,
+    (err) => {
+      if (err) {
+        res.send("数据写入失败");
+      }
+      res.send("数据写入成功");
+    }
+  );
 });
 
 module.exports = router;
